61a69a4c6e36692740223707b5492f53388dd52f
Rico Ullmann <rico@erinnerungsfragmente.de>
2017-09-30 23:04:59 +0200

    streamline needs and triggers

--- items.py
+++ items.py
@@ -9,9 +9,7 @@ files = {
         'source': 'config.fish',
         'content_type': 'mako',
         'mode': '0640',
-        'needs': [
-            'pkg_dnf:fish',
-        ],
+        'needs': ['pkg_dnf:fish'],
     },
 }
 
@@ -20,9 +18,7 @@ actions = {
         'command': 'chsh -s /usr/bin/fish root',
         'unless': 'getent passwd root | cut -d: -f7 | grep /usr/bin/fish',
         'cascade_skip': False,
-        'needs': [
-            'pkg_dnf:fish',
-        ],
+        'needs': ['pkg_dnf:fish'],
     },
 }
 
@@ -31,30 +27,21 @@ if node.metadata.get('fish', {}).get('install_fisherman', True):
         'command': 'curl -Lo ~/.config/fish/functions/fisher.fish --create-dirs git.io/fisherman',
         'unless': 'test -f ~/.config/fish/functions/fisher.fish',
         'cascade_skip': False,
-        'needs': [
-            'pkg_dnf:fish',
-            'pkg_dnf:curl',
-        ],
+        'needs': ['pkg_dnf:fish', 'pkg_dnf:curl'],
     }
     for plugin in node.metadata.get('fish', {}).get('plugins', {}):
         actions['fisher_{}'.format(plugin)] = {
             'command': 'sudo fish -c \'fisher {}\''.format(plugin),
             'unless': 'grep {} ~/.config/fish/fishfile'.format(plugin),
             'cascade_skip': False,
-            'needs': [
-                'pkg_dnf:fish',
-                'action:install_fisherman',
-                'action:enable_fish',
-            ],
+            'needs': ['pkg_dnf:fish', 'action:install_fisherman', 'action:enable_fish'],
         }
 
 for user in node.metadata.get('fish', {}).get('additional_users', {}):
     directories['/home/{}/.config/fish'.format(user)] = {
         'owner': user,
         'group': user,
-        'needs': [
-            'pkg_dnf:fish',
-        ],
+        'needs': ['pkg_dnf:fish'],
     }
     files['/home/{}/.config/fish/config.fish'.format(user)] = {
         'source': 'config.fish',
@@ -62,26 +49,20 @@ for user in node.metadata.get('fish', {}).get('additional_users', {}):
         'owner': user,
         'group': user,
         'content_type': 'mako',
-        'needs': [
-            'pkg_dnf:fish',
-        ],
+        'needs': ['pkg_dnf:fish'],
     }
     actions['enable_fish_{}'.format(user)] = {
         'command': 'chsh -s /usr/bin/fish {}'.format(user),
         'unless': 'getent passwd {} | cut -d: -f7 | grep /usr/bin/fish'.format(user),
         'cascade_skip': False,
-        'needs': [
-            'pkg_dnf:fish',
-        ],
+        'needs': ['pkg_dnf:fish'],
     }
 
     if node.metadata.get('fish', {}).get('install_fisherman', True):
         directories['/home/{}/.config/fisherman'.format(user)] = {
             'owner': user,
             'group': user,
-            'needs': [
-                'pkg_dnf:fish',
-            ],
+            'needs': ['pkg_dnf:fish'],
         }
 
         for plugin in node.metadata.get('fish', {}).get('plugins', {}):
@@ -89,18 +70,11 @@ for user in node.metadata.get('fish', {}).get('additional_users', {}):
                 'command': 'sudo -u {} fish -c \'curl -Lo /home/{}/.config/fish/functions/fisher.fish --create-dirs git.io/fisherman\''.format(user, user),
                 'unless': 'test -f /home/{}/.config/fish/functions/fisher.fish'.format(user),
                 'cascade_skip': False,
-                'needs': [
-                    'pkg_dnf:fish',
-                    'pkg_dnf:curl',
-                ],
+                'needs': ['pkg_dnf:fish', 'pkg_dnf:curl'],
             }
             actions['fisher_{}_{}'.format(user, plugin)] = {
                 'command': 'sudo -u {} fish -c \'fisher {}\''.format(user, plugin),
                 'unless': 'grep {} /home/{}/.config/fish/fishfile'.format(plugin, user),
                 'cascade_skip': False,
-                'needs': [
-                    'pkg_dnf:fish',
-                    'action:install_fisherman_{}'.format(user),
-                    'action:enable_fish_{}'.format(user),
-                ],
+                'needs': ['pkg_dnf:fish', 'action:install_fisherman_{}'.format(user), 'action:enable_fish_{}'.format(user)],
             }
